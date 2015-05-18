b04fba208fe8e8591caf8abb8dedc52fc46bdd0e
vincent78 <vincent78@163.com>
2015-05-18 10:33:55 +0800

    运行环境的切换 iphone->ipad

---
+++ oc-bridge-header.h
@@ -0,0 +1,16 @@
+//
+//  oc-bridge-header.h
+//  sortClass
+//
+//  Created by vincent on 15/5/18.
+//  Copyright (c) 2015年 Fruit. All rights reserved.
+//
+
+#ifndef sortClass_oc_bridge_header_h
+#define sortClass_oc_bridge_header_h
+
+
+#import "OCDevice.h"
+
+
+#endif
---
+++ sortClass/system/OCDevice.h
@@ -0,0 +1,20 @@
+//
+//  OCDevice.h
+//  sortClass
+//
+//  Created by vincent on 15/5/18.
+//  Copyright (c) 2015年 Fruit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCDevice : NSObject
+
+/**
+ *  @brief  获取IP地址
+ *
+ *  @return <#return value description#>
+ */
++ (NSString *)getIPAddress;
+
+@end
